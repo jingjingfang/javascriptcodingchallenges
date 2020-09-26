fde87fe430ad529a005548f5e2cbcdf5f2b993ea
Jingjing Fang <59980749+jingjingfang@users.noreply.github.com>
2020-09-26 19:51:18 -0400

    initial commit

---
+++ PrintEvenAndOddNums/index.js
@@ -0,0 +1,34 @@
+// print the even numberf less than and equal to 20
+let n=20;
+for (let k=0; k<=n;k+=2){
+console.log(k)
+}
+
+
+console.log("**********************************");
+
+// print the odd numberf less than 20
+
+let m=20;
+for (let i=1; i<=m;i+=2){
+console.log(i)
+}
+
+console.log("**********************************");
+// print the odd numberf less than and equal to 20
+function odd (number){
+  for (let i=1;i<=number;i+=2) {
+    console.log(i)
+      }
+}
+
+odd(20);
+
+console.log("**********************************");
+//print the even numberf less than and equal to 20
+function even(input) {
+    for (let i = 0; i <= input; i += 2) {
+        console.log(i);
+    }
+}
+ even(20);
