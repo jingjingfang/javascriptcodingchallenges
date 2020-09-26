516ceb47b34016d990ed7007321ea9a202bd1ec8
Jingjing Fang <59980749+jingjingfang@users.noreply.github.com>
2020-09-26 19:20:49 -0400

    initial commit

---
+++ PrintNums/index.js
@@ -0,0 +1,37 @@
+//print integers from 0-10
+let n=10;
+
+for (let i=0;i<=n; i++){
+    console.log(i);
+}
+
+console.log("__________________________");
+
+//print integers from 0-10
+var num=0;
+var num1=10
+while (num<=num1){
+console.log(num);
+num++;
+}
+
+console.log("__________________________");
+// print integer from -1 to -10
+
+var num2=-1;
+var num3=-10;
+while (num2>num3){
+console.log(num2);
+--num2;
+}
+
+console.log("__________________________");
+
+//print integers from -1-10
+let num4=-10;
+
+for (let i=-1;i>=num4; i--){
+    console.log(i);
+}
+
+
