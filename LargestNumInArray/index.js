b04bd1686e739782bdc0edc6900996f3737462b0
Jingjing Fang <59980749+jingjingfang@users.noreply.github.com>
2020-10-09 21:00:09 -0400

    initial commit

---
+++ LargestNumInArray/index.js
@@ -0,0 +1,10 @@
+// Return an array consisting of the largest number from each provided sub-array.
+
+function largestOfFour(arr) {
+  let result=[];
+  for (let i=0; i<arr.length;i++) {  
+  result.push(Math.max(...arr[i]));
+  }
+  return result;  
+}
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
